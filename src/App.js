952cb73d5342d0ecb6fcb3a3621a19c3c963b6ad
Amdounislim <amdouni.slim@gmail.com>
2020-10-26 17:41:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AddMovie from "./components/AddMovie/AddMovie";
+import MoviesList from "./components/MoviesList";
+import Search from "./components/search/Search";
+import { moviesData } from "./data/MoviesData";
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchRate, setSearchRate] = useState(0);
+
+  const addMovie = (x) => setMovies([...movies, x]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search
+        setSearchInput={setSearchInput}
+        searchRate={searchRate}
+        setSearchRate={setSearchRate}
+      />
+      <MoviesList
+        movies={movies.filter(
+          (el) =>
+            el.rating >= searchRate &&
+            el.name.toUpperCase().includes(searchInput.toUpperCase().trim())
+        )}
+      />
+      <AddMovie addMovie={addMovie} />
     </div>
   );
 }
